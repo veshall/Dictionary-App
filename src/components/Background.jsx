5c218fd3fff361e947700439b885f00d242ef67b
veshall <vishalpaliwal51@gmail.com>
2023-07-05 11:31:20 +0530

    Some UI Changes

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,19 +1,16 @@
 import React from "react";
+import desktopSvg from "../assets/AnimatedShapeDesktop.svg";
+import tabletSvg from "../assets/AnimatedShapeTablet.svg";
+import phoneSvg from "../assets/AnimatedShapephone.svg";
 
 export default function Background() {
   return (
     <picture>
-      <img className="hidden lg:block absolute " src="src\assets\Meteor.svg" />
+      <img className="hidden lg:block absolute " src={desktopSvg} />
 
-      <img
-        className="hidden lg:hidden sm:block absolute"
-        src="src\assets\AnimatedShapeTablet.svg"
-      />
+      <img className="hidden lg:hidden sm:block absolute" src={tabletSvg} />
 
-      <img
-        className="sm:hidden absolute"
-        src="src\assets\AnimatedShapephone.svg"
-      />
+      <img className="sm:hidden absolute" src={phoneSvg} />
     </picture>
   );
 }
