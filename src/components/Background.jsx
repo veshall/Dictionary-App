7b18e1ff617bd16e5adc355c663b8a462de54117
veshall <vishalpaliwal51@gmail.com>
2023-07-05 12:06:01 +0530

    changes

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import desktopSvg from "../assets/AnimatedShapeDesktop.svg";
 import tabletSvg from "../assets/AnimatedShapeTablet.svg";
-import phoneSvg from "../assets/AnimatedShapePhone.svg";
+import PhoneSvg from "../assets/AnimatedShapephone.svg";
 
 export default function Background() {
   return (
     <picture>
       <img className="hidden lg:block absolute " src={desktopSvg} />
       <img className="hidden lg:hidden sm:block absolute" src={tabletSvg} />
-      <img className="sm:hidden absolute" src={phoneSvg} />
+      <img className="sm:hidden absolute" src={PhoneSvg} />
     </picture>
   );
 }
