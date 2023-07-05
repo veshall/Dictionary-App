9f43f3c819ad6751c7c446de75fd07afbd0945ae
veshall <vishalpaliwal51@gmail.com>
2023-07-05 11:19:20 +0530

    background changes

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,10 +3,7 @@ import React from "react";
 export default function Background() {
   return (
     <picture>
-      <img
-        className="hidden lg:block absolute "
-        src="src\assets\AnimatedShapeDesktop.svg"
-      />
+      <img className="hidden lg:block absolute " src="src\assets\Meteor.svg" />
 
       <img
         className="hidden lg:hidden sm:block absolute"
